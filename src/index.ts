acfcb9fba1a2232614918597d4ce7b267c3daa33
Tsvetan Zahariev <102287382+TsvetanZ@users.noreply.github.com>
2022-11-09 19:56:49 +0200

    add server simple

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,15 @@
-//let isOpen = true;
-//
-//isOpen = false;
-//
-//let isOpenArray: boolen[] = [false];
-//
-//
-//
-//function createUser(username: string, age?: number) {
-//    return  {
-//        username,
-//        age
-//    };
-//}
-//
-//const ivan = createUser('Ivan', 12);
+import express from "express";
 
-function id<T>(item: T) : T{
-    return item;
-}
+const app = express();
 
-interface IMyDto {
-    prop: string,
-    prop1: number
-}
+app.get('/', (req, res) => {
+    res.send('Hello WORLD!');
+});
 
-//let num = id<{prop: string; prop1: number }>({prop: '1', prop1: 1}); case without interface
+app.get('/about', (req, res) =>{
+    res.send('About')
+});
 
-type MyDtoOrNumnber = IMyDto | number;
-type BooleanArray = boolean[]; // ili na dolnia   red
-type BoolenArray = Array<boolean>
-
-//let num = id<IMyDto | number>({prop: '1', prop1: 1}); това кагото го няма type MyDtoOrNumnber
-
-let num = id<MyDtoOrNumnber>({prop:'1', prop1:1});
-
-class MyClass {
-    constructor (public name:string, private  age: number) { }
-}
-
-const ivan = new MyClass ('Ivan', 20);
-//(ivan as any ).age = 1000; така прескачаме или пренабрегваме private
-//(ivan as unknown as {test: {best:'123' }}).test.best = 'asdas'; омазване
-
+app.listen(8080, () => {
+    console.log('Servere listening on: 8080')
+});
--- src/test.ts
+++ src/test.ts
@@ -14,18 +14,31 @@
 //}
 //
 //const ivan = createUser('Ivan', 12);
-function id(item) {
+
+function id<T>(item: T) : T{
     return item;
 }
+
+interface IMyDto {
+    prop: string,
+    prop1: number
+}
+
+//let num = id<{prop: string; prop1: number }>({prop: '1', prop1: 1}); case without interface
+
+type MyDtoOrNumnber = IMyDto | number;
+type BooleanArray = boolean[]; // ili na dolnia   red
+type BoolenArray = Array<boolean>
+
 //let num = id<IMyDto | number>({prop: '1', prop1: 1}); това кагото го няма type MyDtoOrNumnber
-var num = id({ prop: '1', prop1: 1 });
-var MyClass = /** @class */ (function () {
-    function MyClass(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    return MyClass;
-}());
-var ivan = new MyClass('Ivan', 20);
+
+let num = id<MyDtoOrNumnber>({prop:'1', prop1:1});
+
+class MyClass {
+    constructor (public name:string, private  age: number) { }
+}
+
+const ivan = new MyClass ('Ivan', 20);
 //(ivan as any ).age = 1000; така прескачаме или пренабрегваме private
 //(ivan as unknown as {test: {best:'123' }}).test.best = 'asdas'; омазване
+
